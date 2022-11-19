e928098722d7690237f5ef5bea298791980326d5
Marek Skrzelowski <marek.skrzelowski@giraffestudio.pl>
2022-11-19 13:58:16 +0100

    Initial Commit

---
+++ GamesForGood/GamesForGoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesForGoodApp.swift
+//  GamesForGood
+//
+//  Created by Marek Skrzelowski on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct GamesForGoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
